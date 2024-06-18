ad759833c9b6f767593b061995bcb5f079974e54
Neto <netovitu@gmail.com>
2024-06-18 14:57:42 -0300

    Adicionando a Query de consulta

--- src/main/java/br/com/api/fatec/apifatec/domain/pedidovenda/PedidoVendaRepository.java
+++ src/main/java/br/com/api/fatec/apifatec/domain/pedidovenda/PedidoVendaRepository.java
@@ -1,9 +1,14 @@
 package br.com.api.fatec.apifatec.domain.pedidovenda;
 
+import br.com.api.fatec.apifatec.entities.Cliente;
 import br.com.api.fatec.apifatec.entities.PedidoVenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PedidoVendaRepository extends JpaRepository<PedidoVenda, Long>, JpaSpecificationExecutor<PedidoVenda> {
-
+    @Query("SELECT p.produto.id, SUM(p.valorTotal) FROM PedidoVendaItem p GROUP BY p.produto.id")
+    List<Object[]> calcularValorTotalPorProduto();
 }
